8945761a4495c6ed55c6fbd6dbb264517fc264a0
moryo <mor.yosef66@gmail.com>
2020-05-18 18:46:28 +0300

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GeneMenuComponent } from './menu/components/gene-menu/gene-menu.component';
 import { GeneChartComponent } from './chart/gene-chart/gene-chart.component';
 import { ChartsModule } from 'ng2-charts';
-import { NgApexchartsModule } from "ng-apexcharts";
 import { ColumnDistributedChartComponent } from './column-distributed-chart/column-distributed-chart.component';
 import { GroupsChartComponent } from './groups-chart/groups-chart.component';
 
@@ -26,8 +25,6 @@ import { GroupsChartComponent } from './groups-chart/groups-chart.component';
     NgxSelectModule,
     ReactiveFormsModule,
     ChartsModule,
-    NgApexchartsModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
