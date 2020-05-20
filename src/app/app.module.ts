b6244cdd9839f85629b13ca67f23e3274266311d
moryo <mor.yosef66@gmail.com>
2020-05-20 13:44:38 +0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { GeneSelectComponent } from './menu/components/gene-select/gene-select.component';
+import { GeneSelectComponent } from './gene-select/gene-select.component'
 import { NgxSelectModule } from 'ngx-select-ex';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { GeneMenuComponent } from './menu/components/gene-menu/gene-menu.component';
 import { GeneChartComponent } from './chart/gene-chart/gene-chart.component';
 import { ChartsModule } from 'ng2-charts';
 import { ColumnDistributedChartComponent } from './column-distributed-chart/column-distributed-chart.component';
@@ -14,7 +13,6 @@ import { GroupsChartComponent } from './groups-chart/groups-chart.component';
   declarations: [
     AppComponent,
     GeneSelectComponent,
-    GeneMenuComponent,
     GeneChartComponent,
     ColumnDistributedChartComponent,
     GroupsChartComponent,    
